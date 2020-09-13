34b171db86747fae14c2cfc57667a0aa1bb74303
AmethystGear <shivenbhatt@outlook.com>
2020-09-13 07:22:28 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,330 @@
+use amethyst::{
+    core::{
+        math::Vector3,
+        timing::Time,
+        transform::{Transform, TransformBundle},
+        Parent,
+    },
+    input::{InputBundle, StringBindings},
+    prelude::*,
+    renderer::{
+        camera::Camera,
+        light,
+        palette::{LinSrgba, Srgb},
+        plugins::{RenderShaded3D, RenderToWindow},
+        rendy::mesh::{Normal, Position, Tangent, TexCoord},
+        shape::Shape,
+        types,
+        visibility::BoundingSphere,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+    window::ScreenDimensions,
+    Error,
+};
+use rand::prelude::*;
+
+use amethyst_nphysics::NPhysicsBackend;
+use amethyst_physics::{prelude::*, PhysicsBundle};
+
+mod components;
+mod systems;
+mod visual_utils;
+
+#[derive(Default)]
+struct Example {
+    time_bank: f32,
+    num_cubes: usize
+}
+
+impl SimpleState for Example {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        // Add light
+        add_light_entity(
+            data.world,
+            Srgb::new(1.0, 1.0, 1.0),
+            Vector3::new(-0.2, -1.0, -0.2),
+            1.0,
+        );
+        add_light_entity(
+            data.world,
+            Srgb::new(1.0, 0.8, 0.8),
+            Vector3::new(0.2, -1.0, 0.2),
+            1.0,
+        );
+
+        // Create floor
+        create_floor(data.world);
+
+        // Create the character + camera.
+        create_character_entity(data.world);
+
+        // Create Box
+        add_cube_entity(data.world, Vector3::new(0.0, 6.0, 0.0));
+    }
+
+    fn update(&mut self, data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        // TODO this code must go inside a System.
+        // Spawn a new cube each X sec.
+        {
+            let time = data.world.fetch::<Time>();
+            self.time_bank += time.delta_seconds();
+        }
+
+        let time_threshold = 2.0;
+        let spawn_scale = 10.0f32; // Scale
+
+        let mut rng = rand::thread_rng();
+
+        while self.time_bank > time_threshold && self.num_cubes < 20 {
+            add_cube_entity(
+                data.world,
+                Vector3::new(
+                    rng.gen::<f32>() * spawn_scale - spawn_scale * 0.5,
+                    6.0,
+                    rng.gen::<f32>() * spawn_scale - spawn_scale * 0.5,
+                ),
+            );
+            self.time_bank -= time_threshold;
+            self.num_cubes += 1;
+        }
+
+        Trans::None
+    }
+}
+
+fn main() -> Result<(), Error> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+
+    let assets_dir = app_root.join("assets");
+    let display_config_path = app_root.join("config").join("display.ron");
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            InputBundle::<StringBindings>::new()
+                .with_bindings_from_file(assets_dir.join("input_bindings.ron"))
+                .unwrap(),
+        )?
+        .with(
+            systems::CameraMotionSystem::new(),
+            "camera_motion_system",
+            &["input_system"],
+        )
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(
+            PhysicsBundle::<f32, NPhysicsBackend>::new()
+                .with_frames_per_seconds(60)
+                .with_max_sub_steps(8) // Safety
+                .with_pre_physics(
+                    systems::CharacterMotionControllerSystem::new(),
+                    String::from("character_motion_controller"),
+                    vec![],
+                ),
+        )?
+        .with_bundle(
+            RenderingBundle::<types::DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)
+                        .unwrap()
+                        .with_clear([0.34, 0.36, 0.52, 1.0]),
+                )
+                .with_plugin(RenderShaded3D::default()),
+        )?;
+    let mut game = Application::build(assets_dir, Example::default())?.build(game_data)?;
+    game.run();
+    Ok(())
+}
+
+fn add_light_entity(world: &mut World, color: Srgb, direction: Vector3<f32>, intensity: f32) {
+    let light: light::Light = light::DirectionalLight {
+        color,
+        direction: direction.normalize(),
+        intensity,
+    }
+    .into();
+
+    world.create_entity().with(light).build();
+}
+
+fn create_floor(world: &mut World) {
+    let shape = {
+        let desc = ShapeDesc::Cube {
+            half_extents: Vector3::new(20.0, 0.2, 20.0),
+        };
+        let physics_world = world.fetch::<PhysicsWorld<f32>>();
+        physics_world.shape_server().create(&desc)
+    };
+
+    let rb = {
+        let mut rb_desc = RigidBodyDesc::default();
+        rb_desc.mode = BodyMode::Static;
+
+        let physics_world = world.fetch::<PhysicsWorld<f32>>();
+        physics_world.rigid_body_server().create(&rb_desc)
+    };
+
+    let mesh = {
+        let mesh_data: types::MeshData = Shape::Cube
+            .generate::<(Vec<Position>, Vec<Normal>, Vec<Tangent>, Vec<TexCoord>)>(Some((
+                20.0, 0.2, 20.0,
+            )))
+            .into();
+
+        visual_utils::create_mesh(world, mesh_data)
+    };
+
+    let mat = visual_utils::create_material(
+        world,
+        LinSrgba::new(0.0, 1.0, 0.0, 1.0),
+        0.0, // Metallic
+        1.0, // Roughness
+    );
+
+    world
+        .create_entity()
+        .with(mesh)
+        .with(mat)
+        .with(BoundingSphere::origin(20.0))
+        .with(Transform::default())
+        .with(shape)
+        .with(rb)
+        .build();
+}
+
+fn add_cube_entity(world: &mut World, pos: Vector3<f32>) {
+    let shape = {
+        let desc = ShapeDesc::Cube {
+            half_extents: Vector3::new(1.0, 1.0, 1.0),
+        };
+        let physics_world = world.fetch::<PhysicsWorld<f32>>();
+        physics_world.shape_server().create(&desc)
+    };
+
+    let rb = {
+        let rb_desc = RigidBodyDesc::default();
+
+        let physics_world = world.fetch::<PhysicsWorld<f32>>();
+        physics_world.rigid_body_server().create(&rb_desc)
+    };
+
+    let mesh = {
+        let mesh_data: types::MeshData = Shape::Cube
+            .generate::<(Vec<Position>, Vec<Normal>, Vec<Tangent>, Vec<TexCoord>)>(Some((
+                1.0, 1.0, 1.0,
+            )))
+            .into();
+
+        visual_utils::create_mesh(world, mesh_data)
+    };
+
+    let mut rng = rand::thread_rng();
+    let mat = visual_utils::create_material(
+        world,
+        LinSrgba::new(rng.gen(), rng.gen(), rng.gen(), rng.gen()),
+        1.0,
+        0.0,
+    );
+
+    let mut transf = Transform::default();
+    transf.set_translation(pos);
+
+    world
+        .create_entity()
+        .with(mesh)
+        .with(mat)
+        .with(BoundingSphere::origin(1.0))
+        .with(transf)
+        .with(shape)
+        .with(rb)
+        .build();
+}
+
+/// Creates three entities:
+/// 1. The character (With RigidBody).
+/// 2. The camera boom handle attached to the character.
+/// 3. The camera attached to the camera bool handle.
+fn create_character_entity(world: &mut World) {
+    let character = {
+        let shape = {
+            let desc = ShapeDesc::Capsule {
+                half_height: 1.0,
+                radius: 0.5,
+            };
+            let physics_world = world.fetch::<PhysicsWorld<f32>>();
+            physics_world.shape_server().create(&desc)
+        };
+
+        let rb = {
+            let mut rb_desc = RigidBodyDesc::default();
+            rb_desc.lock_rotation_x = true;
+            rb_desc.lock_rotation_y = true;
+            rb_desc.lock_rotation_z = true;
+            rb_desc.contacts_to_report = 3;
+            rb_desc.friction = 0.0;
+            rb_desc.bounciness = 0.0;
+
+            let physics_world = world.fetch::<PhysicsWorld<f32>>();
+            physics_world.rigid_body_server().create(&rb_desc)
+        };
+
+        let mesh = {
+            let mesh_data: types::MeshData = Shape::Cube
+                .generate::<(Vec<Position>, Vec<Normal>, Vec<Tangent>, Vec<TexCoord>)>(Some((
+                    0.5, 1.5, 0.5,
+                )))
+                .into();
+
+            visual_utils::create_mesh(world, mesh_data)
+        };
+
+        let mat =
+            visual_utils::create_material(world, LinSrgba::new(0.65, 1.0, 0.90, 1.0), 0.0, 1.0);
+
+        let mut transf = Transform::default();
+        transf.set_translation(Vector3::new(-3.0, 2.0, -3.0));
+
+        world
+            .create_entity()
+            .with(mesh)
+            .with(mat)
+            .with(BoundingSphere::origin(1.0))
+            .with(transf)
+            .with(shape)
+            .with(rb)
+            .with(components::CharacterBody)
+            .build()
+    };
+
+    let camera_boom_handle = {
+        let mut transf = Transform::default();
+        transf.set_translation_y(1.5);
+
+        world
+            .create_entity()
+            .with(transf)
+            .with(components::CameraBoomHandle)
+            .with(Parent { entity: character })
+            .build()
+    };
+
+    let _camera = {
+        let mut camera_transform = Transform::default();
+        camera_transform.set_translation_xyz(0.0, 0.0, 6.0);
+
+        let (width, height) = {
+            let dim = world.read_resource::<ScreenDimensions>();
+            (dim.width(), dim.height())
+        };
+
+        world
+            .create_entity()
+            .with(camera_transform)
+            .with(Camera::standard_3d(width, height))
+            .with(Parent {
+                entity: camera_boom_handle,
+            })
+            .build()
+    };
+}
---
+++ src/systems.rs
@@ -0,0 +1,283 @@
+use amethyst::{
+    core::{
+        math::{Matrix4, UnitQuaternion, Vector3},
+        Transform,
+    },
+    ecs::prelude::*,
+    input::{InputEvent, StringBindings},
+    renderer::Camera,
+    shrev::EventChannel,
+};
+use amethyst_physics::prelude::*;
+
+use crate::components::*;
+
+const MOUSE_SENSITIVITY: f32 = 0.2;
+const MAX_PITCH_ANGLE: f32 = 20.0;
+const FORCE_MULTIPLIER: f32 = 600.0;
+const JUMP_IMPULSE: f32 = 55.0;
+const JUMP_TIME: f32 = 0.25;
+const JUMP_DRAG_POWER: f32 = 5.0;
+const JUMP_MAX_BF: f32 = 0.7;
+
+#[derive(Debug)]
+pub struct CameraMotionSystem {
+    input_event_reader: Option<ReaderId<InputEvent<StringBindings>>>,
+}
+
+impl CameraMotionSystem {
+    pub fn new() -> Self {
+        CameraMotionSystem {
+            input_event_reader: None,
+        }
+    }
+}
+
+impl<'s> System<'s> for CameraMotionSystem {
+    #[allow(clippy::type_complexity)]
+    type SystemData = (
+        ReadExpect<'s, PhysicsTime>,
+        ReadExpect<'s, EventChannel<InputEvent<StringBindings>>>,
+        ReadStorage<'s, CameraBoomHandle>,
+        WriteStorage<'s, Transform>,
+    );
+
+    fn run(
+        &mut self,
+        (physics_time, input_event_channel, camera_boom_handles, mut transforms): Self::SystemData,
+    ) {
+
+        // Capture the input
+        let motion = {
+            let mut m_motion_x = 0.0;
+            let mut m_motion_y = 0.0;
+
+            for e in input_event_channel.read(self.input_event_reader.as_mut().unwrap()) {
+                if let InputEvent::MouseMoved { delta_x, delta_y } = e {
+                    m_motion_x = *delta_y;
+                    m_motion_y = *delta_x * -1.0;
+                    break;
+                }
+            }
+            (
+                m_motion_x * MOUSE_SENSITIVITY,
+                m_motion_y * MOUSE_SENSITIVITY,
+            )
+        };
+
+        for (transform, _) in (&mut transforms, &camera_boom_handles).join() {
+            // Clamp the pitch rotation by avoiding further rotations.
+            let pitch_clamper = {
+                let angles = transform.isometry().rotation.euler_angles();
+
+                let mut pitch_deg = angles.0.to_degrees();
+
+                if angles.2.abs() > std::f32::consts::FRAC_PI_2 {
+                    // Invert the pitch
+                    if pitch_deg < 0.0 {
+                        pitch_deg = pitch_deg + 180.0;
+                    } else {
+                        pitch_deg = pitch_deg - 180.0;
+                    }
+                }
+                if pitch_deg > MAX_PITCH_ANGLE || pitch_deg < -MAX_PITCH_ANGLE {
+                    if pitch_deg.signum() != motion.0.signum() {
+                        1.0
+                    } else {
+                        0.0
+                    }
+                } else {
+                    1.0
+                }
+            };
+
+            let delta_rotation_pitch = UnitQuaternion::from_axis_angle(
+                &Vector3::x_axis(),
+                motion.0 * pitch_clamper * physics_time.delta_seconds(),
+            );
+            let delta_rotation_yaw = UnitQuaternion::from_axis_angle(
+                &Vector3::y_axis(),
+                motion.1 * physics_time.delta_seconds(),
+            );
+
+            transform.isometry_mut().rotation =
+                delta_rotation_yaw * transform.isometry().rotation * delta_rotation_pitch;
+
+            break; // Actually is supported only 1 player
+        }
+    }
+
+    fn setup(&mut self, world: &mut World) {
+        Self::SystemData::setup(world);
+        let mut ie = world.fetch_mut::<EventChannel<InputEvent<StringBindings>>>();
+        self.input_event_reader = Some(ie.register_reader());
+    }
+}
+
+pub struct CharacterMotionControllerSystem {
+    input_event_reader: Option<ReaderId<InputEvent<StringBindings>>>,
+    horizontal_input: Vector3<f32>,
+    vertical_input: f32,
+    jump_time: f32,
+    /// Contact events storage, This motion system is designed to only control a
+    /// single character so store the contacts events here is safe.
+    contact_events: Vec<ContactEvent<f32>>,
+}
+
+impl CharacterMotionControllerSystem {
+    pub fn new() -> Self {
+        Self {
+            input_event_reader: None,
+            horizontal_input: Vector3::zeros(),
+            vertical_input: 0.0,
+            jump_time: 0.0,
+            contact_events: Vec::new(),
+        }
+    }
+}
+
+impl<'s> System<'s> for CharacterMotionControllerSystem {
+    type SystemData = (
+        ReadExpect<'s, PhysicsWorld<f32>>,
+        ReadExpect<'s, PhysicsTime>,
+        ReadExpect<'s, EventChannel<InputEvent<StringBindings>>>,
+        ReadStorage<'s, CharacterBody>,
+        ReadStorage<'s, Camera>,
+        ReadStorage<'s, PhysicsHandle<PhysicsRigidBodyTag>>,
+        ReadStorage<'s, Transform>,
+    );
+
+    fn run(
+        &mut self,
+        (
+            physics_world,
+            physics_time,
+            input_event_channel,
+            character_bodies,
+            cameras,
+            rigid_body_tags,
+            transforms,
+        ): Self::SystemData,
+    ) {
+        for e in input_event_channel.read(self.input_event_reader.as_mut().unwrap()) {
+            if let InputEvent::ActionPressed(action) = e {
+                match action.as_str() {
+                    "Forward" => {
+                        self.horizontal_input.z -= 1.0;
+                    }
+                    "Backward" => {
+                        self.horizontal_input.z += 1.0;
+                    }
+                    "Right" => {
+                        self.horizontal_input.x -= 1.0;
+                    }
+                    "Left" => {
+                        self.horizontal_input.x += 1.0;
+                    }
+                    "Jump" => {
+                        self.vertical_input += 1.0;
+                    }
+                    _ => {}
+                }
+            } else if let InputEvent::ActionReleased(action) = e {
+                match action.as_str() {
+                    "Forward" => {
+                        self.horizontal_input.z += 1.0;
+                    }
+                    "Backward" => {
+                        self.horizontal_input.z -= 1.0;
+                    }
+                    "Right" => {
+                        self.horizontal_input.x += 1.0;
+                    }
+                    "Left" => {
+                        self.horizontal_input.x -= 1.0;
+                    }
+                    "Jump" => {
+                        self.vertical_input -= 1.0;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        let mut camera_pos = Matrix4::<f32>::identity();
+        for (t, _) in (&transforms, &cameras).join() {
+            camera_pos = t.global_matrix().clone();
+        }
+
+        for (body_tag, _) in (&rigid_body_tags, &character_bodies).join() {
+            let is_in_air = {
+                let mut is_in_air = true;
+
+                physics_world
+                    .rigid_body_server()
+                    .contact_events(body_tag.get(), &mut self.contact_events);
+
+                for contact in &self.contact_events {
+                    // Angle in degree between the contact normal and a
+                    // perfectly vertical vector.
+                    let contact_angle = contact
+                        .normal
+                        .dot(&Vector3::new(0.0, 1.0, 0.0))
+                        .acos()
+                        .to_degrees();
+
+                    if contact_angle >= 45.0 {
+                        // Is sliding
+                        // TODO please support sliding
+                    } else {
+                        // Is on ground
+                        is_in_air = false;
+                        break;
+                    }
+                }
+                is_in_air
+            };
+
+            let mut motion_factor = 1.0;
+            let mut breaking_factor = 1.0;
+            if !is_in_air {
+                // On ground
+                // Apply jumping impulse
+                physics_world.rigid_body_server().apply_impulse(
+                    body_tag.get(),
+                    &Vector3::new(0.0, self.vertical_input * JUMP_IMPULSE, 0.0),
+                );
+                self.jump_time = 0.0;
+            }else{
+                // In Air
+                motion_factor = 0.2;
+                self.jump_time += physics_time.delta_seconds() * (1.0 / JUMP_TIME);
+                self.jump_time = self.jump_time.min(1.0);
+                breaking_factor = self.jump_time.powf(JUMP_DRAG_POWER).min(JUMP_MAX_BF);
+            }
+
+            // Apply motion force
+            let mut force = camera_pos.transform_vector(&self.horizontal_input);
+            force.y = 0.0; // Don't apply any force on Y axis
+            physics_world
+                .rigid_body_server()
+                .apply_force(body_tag.get(), &(force * FORCE_MULTIPLIER * motion_factor));
+
+            // Compute breaking force
+            let velocity = physics_world
+                .rigid_body_server()
+                .linear_velocity(body_tag.get());
+
+            let mut bk_force = (velocity / physics_time.delta_seconds()) * -1.0 * breaking_factor;
+            bk_force.y = bk_force.y.min(0.0); // Don't slow down when falling
+            physics_world
+                .rigid_body_server()
+                .apply_force(body_tag.get(), &bk_force);
+
+            break; // Actually only 1 player is allowed;
+        }
+    }
+
+    fn setup(&mut self, world: &mut World) {
+        Self::SystemData::setup(world);
+        let mut ie = world.fetch_mut::<EventChannel<InputEvent<StringBindings>>>();
+        self.input_event_reader = Some(ie.register_reader());
+    }
+}
